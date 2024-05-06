38291661687b2df3cd249d09606765f48ae22af2
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-05-06 15:02:18 -0300

    Não funciona

--- Sobremesas/Sobremesa001.cpp
+++ Sobremesas/Sobremesa001.cpp
@@ -22,7 +22,46 @@ int main() {
 
         switch(escolha) {
             case BENCO:
-                // Código para Benco
+                Benco b;
+	            printf("Escolha o produto desejado: \n001-morango Batavo \n002-tradicional Batavo \n003-morango itambe \n004-tradicional itambe \n005-light morango Batava \n006-light tradicional Batavo \n007-pessego Batavo");
+	           
+			   /* for para verificação de vetor:
+			   
+			   for(int i = 0; i < 7; i++) {
+	       			for(int j = 0; j < 50; j++) {
+	            		printf("%c ", b.nomes_produtos[i][j]);
+	        		}
+	       			 printf("\n");
+	    		} */
+	
+	            cout << "\n\nInsira o codigo do produto: ";
+	                  
+	            cin >> b.codigo_produto;
+	
+	            if (b.codigo_produto == 1 || b.codigo_produto == 2 || b.codigo_produto == 3 || b.codigo_produto == 4 || b.codigo_produto == 7) {
+	                strcpy(b.tipo_chapa1, "chapa azul");
+	                strcpy(b.tipo_chapa2, "chapa azul");
+	            } else if (b.codigo_produto == 6 || b.codigo_produto == 5){
+	                strcpy(b.tipo_chapa1, "chapa verde");
+	                strcpy(b.tipo_chapa2, "chapa verde");
+	            } else {
+	            	cout << "Codigo invalido!\n";
+	            	system("pause");
+	            	system("cls");
+					return main();
+	            	
+					}
+	
+	            cout << "Insira o numero de caixas por producao: ";
+	            cin >> b.caixas_por_producao;
+	
+	            b.total_chapa = b.caixas_por_producao * b.chapa_por_caixa;
+	            b.total_mixpaper = b.caixas_por_producao * b.mixpaper_por_caixa;
+	            b.total_production_time = (float) b.caixas_por_producao / 7.6 /60;
+	
+	            cout << "\nSerao necessarios " << b.total_chapa << " kg de " << b.tipo_chapa1 << endl;
+	            cout << b.total_mixpaper << " kg de " << b.nomes_mixpaper[b.codigo_produto - 1] << " para produzir " << b.caixas_por_producao << " caixas de " << b.nomes_produtos[b.codigo_produto - 1] << endl;
+	            cout << "O tempo necessario para produzir " << b.caixas_por_producao << " caixas de " << b.nomes_produtos[b.codigo_produto -1] << " e de " << b.total_production_time << " horas." << endl;
                 break;
             case PRIMEPACK:
                 // Código para PrimePack
