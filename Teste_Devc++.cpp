900ac658e9ba0f3c771625cf91738f7c0a970cce
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-05-06 13:54:24 -0300

    Update

---
+++ Teste_Devc++.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+int main() {
+    int num1, num2, soma;
+
+    std::cout << "Digite o primeiro número: ";
+    std::cin >> num1;
+
+    std::cout << "Digite o segundo número: ";
+    std::cin >> num2;
+
+    soma = num1 + num2;
+
+    std::cout << "A soma de " << num1 << " e " << num2 << " é " << soma << std::endl;
+
+    return 0;
+}
+
