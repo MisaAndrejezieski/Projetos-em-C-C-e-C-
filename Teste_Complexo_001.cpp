4dd3145bb6b76345da776e285e39f88a2e780763
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-05-06 13:58:39 -0300

    Teste_complexo

---
+++ Teste_Complexo_001.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main() {
+    std::string frase;
+
+    std::cout << "Digite uma frase: ";
+    std::getline(std::cin, frase);
+
+    std::cout << "Frase original: " << frase << std::endl;
+
+    std::reverse(frase.begin(), frase.end());
+
+    std::cout << "Frase invertida: " << frase << std::endl;
+
+    std::transform(frase.begin(), frase.end(), frase.begin(), ::toupper);
+
+    std::cout << "Frase invertida em maiúsculas: " << frase << std::endl;
+
+    return 0;
+}
+
