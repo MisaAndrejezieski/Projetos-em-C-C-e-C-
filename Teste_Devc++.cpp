d53190a0a16f84ed3642888fc9229bb7b659ab9f
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-05-06 13:55:50 -0300

    Correção

--- Teste_Devc++.cpp
+++ Teste_Devc++.cpp
@@ -3,15 +3,15 @@
 int main() {
     int num1, num2, soma;
 
-    std::cout << "Digite o primeiro número: ";
+    std::cout << "Digite o primeiro numero: ";
     std::cin >> num1;
 
-    std::cout << "Digite o segundo número: ";
+    std::cout << "Digite o segundo numero: ";
     std::cin >> num2;
 
     soma = num1 + num2;
 
-    std::cout << "A soma de " << num1 << " e " << num2 << " é " << soma << std::endl;
+    std::cout << "A soma de " << num1 << " e " << num2 << " e " << soma << std::endl;
 
     return 0;
 }
